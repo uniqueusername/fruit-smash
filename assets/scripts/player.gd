ab188d209561fc4dc551eae24a1db71096281733
ary <aryan@jha.io>
2023-09-13 16:53:21 -0700

    added air strafe modifier

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,15 +1,17 @@
 extends CharacterBody2D
 
-@export var JUMP_VELOCITY = -425.0
+# constants
+@export var JUMP_VELOCITY = -400.0
 @export var ACCEL_SPEED = 100
 @export var MAX_SPEED = 300
 @export var DECEL_SPEED = 100
+@export var AIR_SPEED = 0.85
 
 const ROTATE_SPEED = 0.001
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# variables
+var jumping = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# add gravity
@@ -32,12 +34,24 @@ func _physics_process(delta):
 			1:
 				x_input = Input.get_axis("p2_move_left", "p2_move_right")
 				jump_input = Input.is_action_just_pressed("p2_jump")
+				
+		if jump_input and !is_on_floor():
+			jumping = true
+	
+	if is_on_floor():
+		jumping = false
 	
 	# apply inputs
 	if x_input:
-		velocity.x = move_toward(velocity.x, x_input * MAX_SPEED, ACCEL_SPEED)
+		if jumping:
+			velocity.x = move_toward(velocity.x, x_input * MAX_SPEED, ACCEL_SPEED)
+		else:
+			velocity.x = move_toward(velocity.x, x_input * MAX_SPEED, AIR_SPEED * ACCEL_SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, DECEL_SPEED)
+		if jumping:
+			velocity.x = move_toward(velocity.x, 0, DECEL_SPEED)
+		else:
+			velocity.x = move_toward(velocity.x, 0, AIR_SPEED * DECEL_SPEED)
 
 	$Sprite2D.rotation += velocity.x * ROTATE_SPEED
 
@@ -45,3 +59,4 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	move_and_slide()
+	
