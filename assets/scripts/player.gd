822c0e8043154572b526abb5c6263ba1e4bb5609
ary <aryan@jha.io>
2023-09-20 02:53:17 -0700

    v1 grappling hook

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -2,18 +2,23 @@ extends CharacterBody2D
 
 # constants
 @export var JUMP_VELOCITY = -370.0
-@export var ACCEL_SPEED = 100
+@export var ACCEL_SPEED = 25
 @export var MAX_SPEED = 260
-@export var DECEL_SPEED = 35
+@export var DECEL_SPEED = 10
 @export var AIR_SPEED = 0.4
 @export var COYOTE_TIME = 0.1
 @export var HOOK_LENGTH = 100
+@export var CHAIN_PULL = 35
+@export var HOOK_GRAV = 0.5
+@export var HOOK_VERT_FORCE = 1.5
+@export var HOOK_HORIZ_FORCE = 1.0
 
 const ROTATE_SPEED = 0.001
+var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # variables
 var jumping = false
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = GRAVITY
 var coyote_timer 
 var spawn_pos
 var sticky_ray
@@ -24,8 +29,8 @@ func _ready():
 	coyote_timer = COYOTE_TIME
 	spawn_pos = position
 	sticky_ray = $sticky_ray
-	hook = $hook
 	hook_ray = $hook_ray
+	hook = $chain
 
 func _physics_process(delta):
 	# add gravity
@@ -41,7 +46,8 @@ func _physics_process(delta):
 	var aim_input = Vector2.ZERO
 	var jump_input = false
 	var jump_released = false
-	var cast_hook = false
+	var hook_cast = false
+	var hook_released = false
 	
 	if get_meta("mnk_enabled"):
 		# mnk controls
@@ -49,7 +55,8 @@ func _physics_process(delta):
 		aim_input = get_local_mouse_position()
 		jump_input = Input.is_action_just_pressed("ui_up")
 		jump_released = Input.is_action_just_released("ui_up")
-		cast_hook = Input.is_action_just_pressed("mnk_hook")
+		hook_cast = Input.is_action_just_pressed("mnk_hook")
+		hook_released = Input.is_action_just_released("mnk_hook")
 
 	else:
 		# controller controls
@@ -59,13 +66,15 @@ func _physics_process(delta):
 				aim_input = Input.get_vector("p1_aim_left", "p1_aim_right", "p1_aim_up", "p1_aim_down")
 				jump_input = Input.is_action_just_pressed("p1_jump")
 				jump_released = Input.is_action_just_released("p1_jump")
-				cast_hook = Input.is_action_just_pressed("p1_hook")
+				hook_cast = Input.is_action_just_pressed("p1_hook")
+				hook_released = Input.is_action_just_released("p1_hook")
 			1:
 				x_input = Input.get_axis("p2_move_left", "p2_move_right")
 				aim_input = Input.get_vector("p1_aim_left", "p1_aim_right", "p1_aim_up", "p1_aim_down")
 				jump_input = Input.is_action_just_pressed("p2_jump")
 				jump_released = Input.is_action_just_released("p2_jump")
-				cast_hook = Input.is_action_just_pressed("p2_hook")
+				hook_cast = Input.is_action_just_pressed("p2_hook")
+				hook_released = Input.is_action_just_released("p2_hook")
 	
 	aim_input = HOOK_LENGTH / aim_input.length() * aim_input
 	hook_ray.set_target_position(aim_input)
@@ -86,6 +95,18 @@ func _physics_process(delta):
 			velocity.x = move_toward(velocity.x, 0, DECEL_SPEED)
 		else:
 			velocity.x = move_toward(velocity.x, 0, AIR_SPEED * DECEL_SPEED)
+		
+	var chain_velocity := Vector2()
+	if hook.hooked:
+		chain_velocity = to_local(hook.tip).normalized() * CHAIN_PULL
+		chain_velocity.y *= HOOK_VERT_FORCE
+		chain_velocity.x *= HOOK_HORIZ_FORCE
+		# nerf certain inputs if we are hooked
+		gravity = GRAVITY * HOOK_GRAV
+	else:
+		chain_velocity = Vector2(0,0)
+		gravity = GRAVITY
+	velocity += chain_velocity
 
 	# SPIN >:3
 	$Sprite2D.rotation += velocity.x * ROTATE_SPEED
@@ -107,8 +128,10 @@ func _physics_process(delta):
 		velocity.y = 0'''
 		
 	# cast hook
-	if cast_hook:
-		cast_hook()
+	if hook_cast and aim_input.length() > 0.05:
+		hook.shoot(aim_input)
+	if hook_released:
+		hook.release()
 
 	move_and_slide()
 	
@@ -125,8 +148,3 @@ func _process(delta):
 			1:
 				if Input.is_action_just_pressed("p2_reset"):
 					position = spawn_pos
-
-func cast_hook():
-	if hook_ray.is_colliding():
-		if hook_ray.get_collider().get("name") == "walls":
-			hook.global_position = hook_ray.get_collision_point()
