6cfffce4f172f2d19f7ad1b1280a2781fcc8f10a
ary <aryan@jha.io>
2023-09-20 03:46:43 -0700

    updated graphics for hook

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -24,15 +24,13 @@ var spawn_pos
 var sticky_ray
 var hook
 var hook_ray
-var target
 
 func _ready():
 	coyote_timer = COYOTE_TIME
 	spawn_pos = position
 	sticky_ray = $sticky_ray
 	hook_ray = $hook_ray
-	hook = $chain
-	target = $target
+	hook = $chain  
 
 func _physics_process(delta):
 	# add gravity
@@ -80,11 +78,10 @@ func _physics_process(delta):
 	
 	aim_input = HOOK_LENGTH / aim_input.length() * aim_input
 	hook_ray.set_target_position(aim_input)
-	target.rotation = hook_ray.get_target_position().angle() + PI/2
+	$target.rotation = hook_ray.get_target_position().angle() + PI/2
 	$reticle.rotation = hook_ray.get_target_position().angle() + PI/2
-	target.position = hook_ray.get_target_position() * 0.98
+	$target.position = hook_ray.get_target_position() * 0.98
 	$reticle.position = hook_ray.get_target_position()
-
 	
 	# reset coyote timer when we land
 	if is_on_floor():
