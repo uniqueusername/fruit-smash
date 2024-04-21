36a7b24445ae2502a87cc20ef126b64943daf24d
ary <aryan@jha.io>
2024-04-20 19:20:40 -0700

    fix disable on level start

--- assets/scripts/game_timer.gd
+++ assets/scripts/game_timer.gd
@@ -1,5 +1,11 @@
 extends Label
 
+@onready var players = get_parent().get_node("players")
+
+func _ready():
+	players.get_node("player").set_disabled(true)
+	players.get_node("player2").set_disabled(true)
+
 func _process(delta):
 	var tl = int($Timer.time_left)
 	if tl == 0:
@@ -8,4 +14,6 @@ func _process(delta):
 		text = str(int($Timer.time_left))
 
 func _on_timer_timeout():
+	players.get_node("player").set_disabled(false)
+	players.get_node("player2").set_disabled(false)
 	queue_free()
