723585c01063471e261e42f2733bc934a0a1fd1e
ary <aryan@jha.io>
2024-06-16 22:40:15 -0700

    basic server infra :shiina:

--- scenes/objects/utility/network.gd
+++ scenes/objects/utility/network.gd
@@ -1,32 +1,32 @@
 extends Node
 
-signal player_connected(peer_id, player_info)
-signal player_disconnected(peer_id)
-signal server_disconnected
+# server config
+const PORT: int = 7000
+const MAX_CONNECTIONS: int = 4
+const DEFAULT_ADDRESS: String = "127.0.0.1"
 
-const PORT = 7000
-const MAX_CONNECTIONS = 4
-const DEFAULT_ADDRESS = "127.0.0.1"
+# player config
+const DEFAULT_NAME: String = "michael soft"
+var player_info = { "name": DEFAULT_NAME }
 
-const DEFAULT_NAME = "michael soft"
-
+# runtime vars
 var players = {}
 
-var player_info = {"name": DEFAULT_NAME}
-
-func _ready():	
-	multiplayer.peer_connected.connect(_on_player_connected)
-	multiplayer.peer_disconnected.connect(_on_player_disconnected)
+func _ready():
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
 	multiplayer.connected_to_server.connect(_on_connected_ok)
 	multiplayer.connection_failed.connect(_on_connected_fail)
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
 
-func start_lobby():
+func host_lobby():
 	var peer = ENetMultiplayerPeer.new()
 	var error = peer.create_server(PORT, MAX_CONNECTIONS)
 	if error: return error
 	multiplayer.multiplayer_peer = peer
 	
+	_distribute_player_info(multiplayer.get_unique_id(), player_info)
+
 func join_lobby(address = ""):
 	if address.is_empty(): address = DEFAULT_ADDRESS
 	var peer = ENetMultiplayerPeer.new()
@@ -34,23 +34,21 @@ func join_lobby(address = ""):
 	if error: return error
 	multiplayer.multiplayer_peer = peer
 
-func _on_player_connected(id):
+@rpc("any_peer", "call_local")
+func start_game():
+	get_tree().change_scene_to_file("res://scenes/maps/dev_map.tscn")
+
+# called when any new peer joins the network
+func _on_peer_connected(id):
 	_register_player.rpc_id(id, player_info)
 
-@rpc("any_peer", "reliable")
-func _register_player(new_player_info):
-	var new_player_id = multiplayer.get_remote_sender_id()
-	players[new_player_id] = new_player_info
-	player_connected.emit(new_player_id, new_player_info)
-
-func _on_player_disconnected(id):
+func _on_peer_disconnected(id):
 	players.erase(id)
-	player_disconnected.emit(id)
 
+# called when *this peer* connects to a server
 func _on_connected_ok():
 	var peer_id = multiplayer.get_unique_id()
 	players[peer_id] = player_info
-	player_connected.emit(peer_id, player_info)
 
 func _on_connected_fail():
 	multiplayer.multiplayer_peer = null
@@ -58,4 +56,17 @@ func _on_connected_fail():
 func _on_server_disconnected():
 	multiplayer.multiplayer_peer = null
 	players.clear()
-	server_disconnected.emit()
+
+@rpc("any_peer", "reliable")
+func _register_player(new_player_info):
+	var new_player_id = multiplayer.get_remote_sender_id()
+	players[new_player_id] = new_player_info
+	_distribute_player_info(new_player_id, new_player_info)
+
+@rpc("any_peer", "call_remote", "reliable")
+func _distribute_player_info(id, info):
+	players[id] = info
+	
+	if multiplayer.is_server():
+		for i in players:
+			_distribute_player_info.rpc(i, players[i])
